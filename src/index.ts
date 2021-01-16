bcf40b5df44550f732ee2ca49ba4217a80d96dbb
Miqueias Sousa <miqueiassousa80@gmail.com>
2021-01-16 20:03:29 -0300

    Add cors

--- src/index.ts
+++ src/index.ts
@@ -1,6 +1,10 @@
 import express from 'express'
+import cors from 'cors'
 
 const app = express()
+const origin = process.env.NODE_ENV === 'production' ? process.env.ALLOW_ORIGIN : '*'
 const PORT = process.env.PORT || 5000
+
+app.use(cors({ origin }))
 
 app.listen(PORT, () => console.log(`[App]: Listening on port ${PORT}`))
