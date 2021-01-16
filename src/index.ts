c0b49211f25a2c7537113da646b6357048e7beca
Miqueias Sousa <miqueiassousa80@gmail.com>
2021-01-16 20:05:48 -0300

    Add morgan

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,12 @@
 import express from 'express'
 import cors from 'cors'
+import morgan from 'morgan'
 
 const app = express()
 const origin = process.env.NODE_ENV === 'production' ? process.env.ALLOW_ORIGIN : '*'
 const PORT = process.env.PORT || 5000
 
 app.use(cors({ origin }))
+app.use(morgan('dev'))
 
 app.listen(PORT, () => console.log(`[App]: Listening on port ${PORT}`))
