f98ff5a50699dee9bf4293402e964ea4f1c154a9
Miqueias Sousa <miqueiassousa80@gmail.com>
2021-01-20 22:34:46 -0300

    Create migration points

---
+++ src/database/migrations/20210120214947_create_points.ts
@@ -0,0 +1,20 @@
+import * as Knex from 'knex'
+
+export async function up (knex: Knex): Promise<void> {
+  return knex.schema.createTable('points', table => {
+    table.increments('id')
+    table.string('name').notNullable().unique()
+    table.string('email').notNullable().unique()
+    table.integer('phone_number').notNullable().unique()
+    table.string('image_url').notNullable().unique()
+    table.integer('user_id').notNullable().references('id').inTable('users')
+    table.string('address').notNullable().unique()
+    table.string('city').notNullable()
+    table.string('state', 2).notNullable()
+    table.timestamps(true, true)
+  })
+}
+
+export async function down (knex: Knex): Promise<void> {
+  return knex.schema.dropTable('points')
+}
