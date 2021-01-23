f3fac0e0de52985308892c95d9dc4fe973ba488f
Miqueias Sousa <miqueiassousa80@gmail.com>
2021-01-22 22:08:29 -0300

    Create routes entrypoint

--- src/app.ts
+++ src/app.ts
@@ -2,11 +2,15 @@ import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
 
+import routes from './routes'
+
 const app = express()
 const origin = process.env.NODE_ENV === 'production' ? process.env.ALLOW_ORIGIN : '*'
 
 app.use(cors({ origin }))
 app.use(morgan('dev'))
 app.use(express.json())
+
+app.use('/', routes)
 
 export default app
