5f2b239c9aff65fadac9eb9da05c7b64f24d1baf
Miqueias Sousa <miqueiassousa80@gmail.com>
2021-01-26 21:55:30 -0300

    Create User Repository

---
+++ src/database/repository/UserRepo.ts
@@ -0,0 +1,22 @@
+import knex from '../../utils/knex'
+import { IUser, IUserDTO } from '../../types/user'
+
+class UserRepo {
+  static async create (userData: IUserDTO) {
+    const user = await knex<IUser>('users')
+      .insert(userData)
+      .returning('*')
+
+    return user
+  }
+
+  static async findByOne (entrie: Partial<IUser>) {
+    const user = await knex<IUser>('users')
+      .where(entrie)
+      .first()
+
+    return user
+  }
+}
+
+export default UserRepo
