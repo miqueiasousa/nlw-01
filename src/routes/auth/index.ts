087ceeef267d5818038a35f5b7c3ec7dd5ce0685
Miqueias Sousa <miqueiassousa80@gmail.com>
2021-01-29 22:25:43 -0300

    Move user register to signup route

---
+++ src/routes/auth/index.ts
@@ -0,0 +1,23 @@
+import express from 'express'
+
+import UserRepo from '../../database/repository/UserRepo'
+
+const router = express.Router()
+
+router.post('/signup', async (req, res) => {
+  const user = await UserRepo.findOne({ email: req.body.email })
+
+  if (user) {
+    throw new Error('User email is already declared')
+  }
+
+  const createdUser = await UserRepo.create({
+    name: req.body.name,
+    email: req.body.email,
+    password: req.body.password
+  })
+
+  return res.status(201).json(createdUser)
+})
+
+export default router
--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -14,20 +14,4 @@ router.get('/:id', async (req, res) => {
   return res.status(200).json(user)
 })
 
-router.post('/', async (req, res) => {
-  const user = await UserRepo.findOne({ email: req.body.email })
-
-  if (user) {
-    throw new Error('User email is already declared')
-  }
-
-  const createdUser = await UserRepo.create({
-    name: req.body.name,
-    email: req.body.email,
-    password: req.body.password
-  })
-
-  return res.status(201).json(createdUser)
-})
-
 export default router
