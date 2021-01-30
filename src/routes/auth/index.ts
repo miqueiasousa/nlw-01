05906ff4c01eb2728c172652f3e59c9ef3d64576
Miqueias Sousa <miqueiassousa80@gmail.com>
2021-01-29 22:42:41 -0300

    Implements hash user password

--- src/routes/auth/index.ts
+++ src/routes/auth/index.ts
@@ -1,8 +1,10 @@
 import express from 'express'
+import bcrypt from 'bcrypt'
 
 import UserRepo from '../../database/repository/UserRepo'
 
 const router = express.Router()
+const SALT_ROUNDS = 10
 
 router.post('/signup', async (req, res) => {
   const user = await UserRepo.findOne({ email: req.body.email })
@@ -11,10 +13,11 @@ router.post('/signup', async (req, res) => {
     throw new Error('User email is already declared')
   }
 
+  const hashedPassword = await bcrypt.hash(req.body.password, SALT_ROUNDS)
   const createdUser = await UserRepo.create({
     name: req.body.name,
     email: req.body.email,
-    password: req.body.password
+    password: hashedPassword
   })
 
   return res.status(201).json(createdUser)
