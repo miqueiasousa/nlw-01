9ec0d29ab7fc9c79a45bec71a7be1e86d7448be0
Miqueias Sousa <miqueiassousa80@gmail.com>
2021-01-29 22:22:19 -0300

    Create route to show user

--- src/routes/user/index.ts
+++ src/routes/user/index.ts
@@ -4,6 +4,16 @@ import UserRepo from '../../database/repository/UserRepo'
 
 const router = express.Router()
 
+router.get('/:id', async (req, res) => {
+  const user = await UserRepo.findOne({ id: Number(req.params.id) })
+
+  if (!user) {
+    throw new Error('User not found')
+  }
+
+  return res.status(200).json(user)
+})
+
 router.post('/', async (req, res) => {
   const user = await UserRepo.findOne({ email: req.body.email })
 
