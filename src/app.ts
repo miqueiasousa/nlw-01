13e704bdbfed1b27555652608f036f5e6584a071
Miqueias Sousa <miqueiassousa80@gmail.com>
2021-02-08 22:13:58 -0300

    Add helmet

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express'
 import cors from 'cors'
 import morgan from 'morgan'
+import helmet from 'helmet'
 
 import routes from './routes'
 import errorHandler from './middlewares/errorHandler'
@@ -8,6 +9,7 @@ import errorHandler from './middlewares/errorHandler'
 const app = express()
 const origin = process.env.NODE_ENV === 'production' ? process.env.ALLOW_ORIGIN : '*'
 
+app.use(helmet())
 app.use(cors({ origin }))
 app.use(morgan('dev'))
 app.use(express.json())
