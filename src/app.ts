c688772e052906d09fd96c48ccbfc4a419bfcdb3
Miqueias Sousa <miqueiassousa80@gmail.com>
2021-02-17 21:45:07 -0300

    Delete bodyParserErrorHandler middleware

--- src/app.ts
+++ src/app.ts
@@ -5,7 +5,6 @@ import helmet from 'helmet'
 
 import routes from './routes'
 import errorHandler from './middlewares/errorHandler'
-import bodyParserErrorHandler from './middlewares/bodyParserErrorHandler'
 
 const app = express()
 const origin = process.env.NODE_ENV === 'production' ? process.env.ALLOW_ORIGIN : '*'
@@ -14,7 +13,6 @@ app.use(helmet())
 app.use(cors({ origin }))
 app.use(morgan('dev'))
 app.use(express.json())
-app.use(bodyParserErrorHandler())
 
 app.use('/', routes)
 
